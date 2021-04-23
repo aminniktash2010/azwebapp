9c403c658085113e42b470982f1522eb37fbddd1
aminniktash2010 <80291034+aminniktash2010@users.noreply.github.com>
2021-04-23 13:10:53 -0400

    1

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
 //--------------------------------------------------------------------
 // Modules
 module "demo" {

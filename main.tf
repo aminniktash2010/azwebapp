51e0cfc45356a96f97450423ea8c362986f5cd99
amin <newaminazur@outlook.com>
2021-04-23 03:26:18 -0400

    01

--- main.tf
+++ main.tf
@@ -2,12 +2,23 @@
 terraform {
   required_version = ">= 0.12"
 }
+
+
 # Configure the Azure provider
 provider "azurerm" {
   environment = "public"
   features {}
-}
+}}
+
 module "webapp" {
   source = "./modules/webapp"
-  env = "var.enviroment"
+  location    = "canadacentral"
+  company     = "amindev"
+  environment = "dev"
+  owner       = "Amin Niktash"
+  description = "Linux App Service with .NET Core"
+  region      = "ne"
+  app_name    = "azapp01"
+
   }
+}}

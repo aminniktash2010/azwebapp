5bdde7773a6e5e7ed6334ee75e1900f4bdf26a74
aminniktash2010 <80291034+aminniktash2010@users.noreply.github.com>
2021-04-23 02:15:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,11 @@
 terraform {
   required_version = ">= 0.12"
 }
-
-
 # Configure the Azure provider
 provider "azurerm" {
   environment = "public"
   features {}
 }
-
 module "webapp" {
   source = "./modules/webapp"
 

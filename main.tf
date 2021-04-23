0b288c6af7ea44f9cd2331dcbada51a7c21e86e8
aminniktash2010 <80291034+aminniktash2010@users.noreply.github.com>
2021-04-23 14:25:37 -0400

    1

--- main.tf
+++ main.tf
@@ -22,3 +22,22 @@ module "demo" {
   environment = "dev"
   owner = "amin"
 }
+
+//--------------------------------------------------------------------
+// Modules
+module "nginx" {
+  source  = "app.terraform.io/nik1/nginx/module"
+  version = "1.0.0"
+
+  company = "nik"
+  description = "nginx"
+  environment = "dev"
+  location = "canadacentral"
+  network-vnet-cidr = "10.10.0.0/16"
+  nginx_vm_size = "Standard_B2s"
+  owner = "Amin Niktash"
+  prefix = "az"
+  vm-subnet-cidr = "10.10.1.0/24"
+}
+
+

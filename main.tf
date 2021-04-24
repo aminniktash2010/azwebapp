f4443ddc86edf3da1467004160fa2860e7d5d629
aminniktash2010 <80291034+aminniktash2010@users.noreply.github.com>
2021-04-23 23:46:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ module "nginx" {
 // Modules
 module "sql" {
   source  = "app.terraform.io/nik1/sql/module"
-  version = "1.0.3"
+  version = "1.0.4"
   # insert required variables here
   sql_server_name         = "amin-sqlsrv01"
   sql_database_name       = "parsa-sql"
